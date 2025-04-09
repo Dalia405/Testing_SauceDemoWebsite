9e6d84ee61ee2f744645aedf8ebdac1e6df38ac6
Abdullah Moghazy <78924913+AbdullahMoghazy@users.noreply.github.com>
2025-04-09 17:00:44 +0200

    Update AddtoCart.java

--- src/test/java/tests/home/AddtoCart.java
+++ src/test/java/tests/home/AddtoCart.java
@@ -1,39 +1,33 @@
+package tests.cart;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterTest;
 
 import java.util.List;
 
-public class AddtoCart {
+import tests.base.BaseTest;
 
-    WebDriver driver;
-
-    @BeforeTest
-    public void setUp() {
-        driver = new EdgeDriver();
-    }
+public class AddtoCart extends BaseTest {
 
     @Test
     public void addProductsToCart() {
         try {
+            WebDriver driver = getDriver(); 
+
             driver.get("https://www.saucedemo.com/");
             driver.manage().window().maximize();
 
-            // Login
             driver.findElement(By.id("user-name")).sendKeys("standard_user");
             driver.findElement(By.id("password")).sendKeys("secret_sauce");
             driver.findElement(By.id("login-button")).click();
 
-            Thread.sleep(2000); // wait for login
+            Thread.sleep(2000); 
 
-            // Find all "Add to cart" buttons
             List<WebElement> addToCartButtons = driver.findElements(By.xpath("//button[text()='Add to cart']"));
 
-            // Click first 6 buttons (or fewer)
             for (int i = 0; i < Math.min(6, addToCartButtons.size()); i++) {
                 System.out.println("Adding product to cart: " + addToCartButtons.get(i).getAttribute("name"));
                 addToCartButtons.get(i).click();
@@ -47,8 +41,9 @@ public class AddtoCart {
 
     @AfterTest
     public void tearDown() {
+        WebDriver driver = getDriver();
         if (driver != null) {
-            driver.quit();
+            driver.quit(); 
         }
     }
 }
