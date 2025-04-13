b397deb9ace0b519eacd3dc080e5ee3e4e26e6e2
Abdullah Moghazy <78924913+AbdullahMoghazy@users.noreply.github.com>
2025-04-13 11:34:57 +0200

    Update AddToCart.java

--- src/test/java/tests/home/AddToCart.java
+++ src/test/java/tests/home/AddToCart.java
@@ -12,20 +12,42 @@ import java.util.List;
 import tests.base.BaseTest;
 
 public class AddToCart extends BaseTest {
-
-    @Test
+    int addedProductsCount = 0;
+    @Test(priority = 0)
     public void addProductsToCart() {
         try {
             List<WebElement> addToCartButtons = driver.findElements(By.xpath("//button[text()='Add to cart']"));
             for (int i = 0; i < Math.min(6, addToCartButtons.size()); i++) {
                 System.out.println("Adding product to cart: " + addToCartButtons.get(i).getAttribute("name"));
                 addToCartButtons.get(i).click();
+                addedProductsCount++;
                 Thread.sleep(1000);
             }
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("window.scrollTo(0, 0);");
         }
         catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test(priority = 1)
+    public void VerifyCartCount(){
+        try{
+            WebElement cartBadge = driver.findElement(By.className("shopping_cart_badge"));
+            String badgeText = cartBadge.getText();
+            int badgeCount = Integer.parseInt(badgeText);
+
+            System.out.println("Expected cart count: " + addedProductsCount);
+            System.out.println("Cart badge shows: " + badgeCount);
+
+            if (badgeCount == addedProductsCount) {
+                System.out.println("Cart count matches the number of added products.");
+            } else {
+                System.out.println("Cart count does NOT match the number of added products.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to verify cart badge.");
             e.printStackTrace();
         }
     }
